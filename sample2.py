13c4f69e4327dddca48a4e43e838e567034aef03
priyankavaidya261 <81521391+priyankavaidya261@users.noreply.github.com>
2022-12-13 15:56:38 +0530

    Add files via upload

---
+++ sample2.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from functools import partial
+
+def validateLogin(username,password):
+    print("username entered:", username.get())
+    print("password entered:",password.get())
+    return
+
+tkWindow = Tk()
+tkWindow.geometry("400x400")
+tkWindow.title('Tkinter login form -python example.org')
+
+usernameLabel = Label(tkWindow,text ="User Name").grid(row =0,column=0)
+username = StringVar()
+usernameEntry = Entry(tkWindow,textvariable = username).grid(row=0,column=1)
+
+passwordLabel = Label(tkWindow,text="Password").grid(row=1,column=0)
+password = StringVar()
+passwordEntry = Entry(tkWindow, textvariable = password,show = '*').grid(row = 1,column=1)
+
+validateLogin = partial(validateLogin,username,password)
+loginButton = Button(tkWindow,text ="Login",command = validateLogin).grid(row =4,column =0)
+tkWindow.mainloop()
